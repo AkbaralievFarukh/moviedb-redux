0e4a7ffbe9118953768fb745a5851d4fa1bd65d9
Акбаралиев Фарух <akbfarukh@gmail.com>
2023-12-11 17:32:12 +0600

    initial

--- src/Components/MovieItem/MovieItem.js
+++ src/Components/MovieItem/MovieItem.js
@@ -5,7 +5,7 @@ import './MovieItem.css'
 
 const MovieItem = ({movie}) => {
     return (
-        <div className={"col-4"}>
+        <div className={"col-3"}>
             <div className={"card"}>
                 <img className={"card-img"} src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} alt={movie.title}/>
                 <div className={"card-body"}>
