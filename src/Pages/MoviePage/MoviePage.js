08dc93f935384c65c6a2f9414bd6fe6133ec6a3f
Акбаралиев Фарух <akbfarukh@gmail.com>
2023-12-12 17:41:38 +0600

    modifided

--- src/Pages/MoviePage/MoviePage.js
+++ src/Pages/MoviePage/MoviePage.js
@@ -3,7 +3,7 @@ import {useDispatch, useSelector} from "react-redux";
 import {useNavigate, useParams} from "react-router-dom";
 import {clearMovie, getMovieById} from "../../Redux/Actions/MovieAction";
 import './MoviePage.css'
-import {Container} from "@mui/material";
+import {Container, Grid, Typography} from "@mui/material";
 
 const MoviePage = () => {
     const dispatch = useDispatch();
@@ -21,23 +21,17 @@ const MoviePage = () => {
     }, [dispatch, navigate, id]);
     return (
         <Container >
-            <div className={"movie-card"}>
-                <div className={"row"}>
-                    <div className={"col-6"}>
-                        <div className={"movie-box"}>
-                            <img src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} alt={movie.title} />
-                        </div>
-                    </div>
-                    <div className={"col-6"}>
-                        <div className={"movie-box"}>
-                            <h1 className={"movie-title"}>{movie.title}</h1>
-                            <p className={"movie-overview"}>{movie.overview}</p>
-                            <p className={"movie-date"}>Дата выхода: {movie.release_date}</p>
-                            <p className={"movie-rating"}>Рейтинг: {movie.vote_average}</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <Grid container spacing={2} sx={{marginTop: '30px', marginBottom: '30px'}}>
+                <Grid item xs={12} md={6}>
+                    <img className={"movie-poster"} src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} alt={movie.title} />
+                </Grid>
+                <Grid item xs={12} md={6} >
+                    <Typography sx={{ marginBottom: '20px' }} variant={"h4"} >{movie.title}</Typography>
+                    <Typography sx={{ marginBottom: '20px' }} variant={"subtitle1"} >{movie.overview}</Typography>
+                    <Typography sx={{ marginBottom: '20px' }} variant={"subtitle2"} >Дата выхода: {movie.release_date}</Typography>
+                    <Typography sx={{ marginBottom: '20px' }} variant={"subtitle2"} >Рейтинг: {movie.vote_average}</Typography>
+                </Grid>
+            </Grid>
         </Container>
     );
 };
